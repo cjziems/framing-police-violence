0626087a3a1bc6cd65df051eeb9e57b698c4152c
Caleb John Ziems <cziems3@panda.cc.gatech.edu>
2022-04-02 01:03:56 -0400

    pipeline

---
+++ populate_repo.sh
@@ -0,0 +1,9 @@
+mkdir -p data/raw/shootings
+mkdir -p data/prepared/shootings
+mkdir frames-extracted
+
+mkdir -p resources/moral_foundations
+wget https://databank.illinois.edu/datafiles/pjwpj/download -O resources/moral_foundations/Enhanced_Morality_Lexicon_V1.1.txt
+wget https://www.dropbox.com/s/hu9gt3znr6uaz8f/data.zip?dl=0
+unzip data.zip
+
